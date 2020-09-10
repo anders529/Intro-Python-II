aefe85b1383c00d271a3568749eb0902fff14d75
Aaron Andersen <aaronandersen@zohomail.com>
2020-09-10 15:52:57 -0500

    Finished MVP

--- src/adv.py
+++ src/adv.py
@@ -1,5 +1,5 @@
 from room import Room
-
+from player import Player
 # Declare all the rooms
 
 room = {
@@ -20,10 +20,8 @@ to north. The smell of gold permeates the air."""),
 chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
 }
-blah blah blah something something something
 
 # Link rooms together
-
 room['outside'].n_to = room['foyer']
 room['foyer'].s_to = room['outside']
 room['foyer'].n_to = room['overlook']
@@ -32,20 +30,64 @@ room['overlook'].s_to = room['foyer']
 room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
-
 #
 # Main
 #
 
 # Make a new player object that is currently in the 'outside' room.
+player = Player(input("What's your name?: "), room["outside"])
 
 # Write a loop that:
-#
-# * Prints the current room name
-# * Prints the current description (the textwrap module might be useful here).
-# * Waits for user input and decides what to do.
-#
-# If the user enters a cardinal direction, attempt to move to the room there.
-# Print an error message if the movement isn't allowed.
-#
-# If the user enters "q", quit the game.
+while True:
+    try:
+        # * Prints the current room name
+        print(f"\nCurrent room: {player.current_room.name}")
+
+        # * Prints the current description (the textwrap module might be useful here).
+        print(f"\nRoom description: {player.current_room.description}")
+
+        # * Waits for user input and decides what to do.
+        commands = ["n - north", "s - south", "e - east", "w - west", "q - quit"]
+        print("\nCommands are as follows...")
+        for i in commands:
+            print(i)
+
+        print("\n")
+        command = input("\nChoose a command: ")
+        print("\n")
+
+        # If the user enters "q", quit the game.
+        if command == "q":
+            print("\n")
+            print(f"\nThanks for playing {player.name}")
+            print("\n")
+            break
+
+        # If the user enters a cardinal direction, attempt to move to the room there.
+        if command == "n":
+            if player.current_room.n_to == None:
+                print("\n The way through is blocked find another way")
+            else:
+                player.change_room(player.current_room.n_to)
+
+        if command == "s":
+            if player.current_room.s_to == None:
+                print("\nThe way through is blocked find another way")
+            else:
+                player.change_room(player.current_room.s_to)
+
+        if command == "e":
+            if player.current_room.e_to == None:
+                print("\n The way through is blocked find another way")
+            else:
+                player.change_room(player.current_room.e_to)
+
+        if command == "w":
+            if player.current_room.w_to == None:
+                print("\nThe way through is blocked find another way")
+            else:
+                player.change_room(player.current_room.w_to)
+
+    # Print an error message if the movement isn't allowed.
+    except AttributeError:
+        print("Something must have went wrong")
--- src/player.py
+++ src/player.py
@@ -1,2 +1,12 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class Player:
+  def __init__(self, name, current_room):
+    self.name = name
+    self.current_room = current_room
+
+  def __str__(self):
+    return f"{self.name}'s current location is the {self.current_room.name} room"
+
+  def change_room(self, room):
+        self.current_room = room
